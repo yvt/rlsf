3c4fa6436110c1329455600a72dc701b307d3989
yvt <i@yvt.jp>
2021-04-04 20:50:52 +0900

    doc: add `README.md`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,54 @@
+//! This crate implements the TLSF (Two-Level Segregated Fit) dynamic memory
+//! allocation algorithm¹.
+//!
+//!  - **Allocation and deallocation operations are guaranteed to complete in
+//!    constant time.** TLSF is suitable for real-time applications.
+//!
+//!  - **The memory pool is provided by an application.** Examples of potential
+//!    memory pool sources include: a `static` array for global memory
+//!    allocation, a memory block allocated by another memory allocator for
+//!    arena allocation.
+//!
+//!  - **This crate supports `#![no_std]`.** It can be used in bare-metal and
+//!    RTOS-based applications.
+//!
+//! <!-- <small> doesn't work on GitHub -->
+//!
+//! <sub>¹ M. Masmano, I. Ripoll, A. Crespo and J. Real, "TLSF: a new dynamic
+//! memory allocator for real-time systems," *Proceedings. 16th Euromicro
+//! Conference on Real-Time Systems*, 2004. ECRTS 2004., Catania, Italy, 2004,
+//! pp. 79-88, doi: 10.1109/EMRTS.2004.1311009.</sub>
+//!
+//! # Examples
+//!
+//! ```rust
+//! use rlsf::Tlsf;
+//! use std::{mem::MaybeUninit, alloc::Layout};
+//!
+//! let mut pool = [MaybeUninit::uninit(); 65536];
+//!
+//! // On 32-bit systems, the maximum block size is 16 << FLLEN = 65536 bytes.
+//! // The worst-case fragmentation is (16 << FLLEN) / SLLEN - 2 = 4094 bytes.
+//! // `'pool` represents the memory pool's lifetime (`pool` in this case).
+//! let mut tlsf: Tlsf<'_, u16, u16, 12, 16> = Tlsf::INIT;
+//! //                 ^^            ^^  ^^
+//! //                  |             |  |
+//! //                'pool           |  SLLEN
+//! //                               FLLEN
+//! tlsf.insert_free_block(&mut pool);
+//!
+//! unsafe {
+//!     let mut ptr1 = tlsf.allocate(Layout::new::<u64>()).unwrap().cast::<u64>();
+//!     let mut ptr2 = tlsf.allocate(Layout::new::<u64>()).unwrap().cast::<u64>();
+//!     *ptr1.as_mut() = 42;
+//!     *ptr2.as_mut() = 56;
+//!     assert_eq!(*ptr1.as_ref(), 42);
+//!     assert_eq!(*ptr2.as_ref(), 56);
+//!     tlsf.deallocate(ptr1.cast(), Layout::new::<u64>().align());
+//!     tlsf.deallocate(ptr2.cast(), Layout::new::<u64>().align());
+//! }
+//! ```
+//!
 #![no_std]
 
 // FIXME: panicking in constants is unstable
