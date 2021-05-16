2016ed2e7fe32c17b5fcd8e20107f0e6fc8a3fcb
yvt <i@yvt.jp>
2021-05-16 22:31:10 +0900

    test: add another `FlexSource` used for testing

--- crates/rlsf/src/flex/tests.rs
+++ crates/rlsf/src/flex/tests.rs
@@ -1,5 +1,5 @@
 use quickcheck_macros::quickcheck;
-use std::prelude::v1::*;
+use std::{fmt, prelude::v1::*};
 
 use super::*;
 use crate::{
@@ -59,11 +59,63 @@ unsafe impl<T: FlexSource> FlexSource for TrackingFlexSource<T> {
     }
 }
 
+/// Continuous-growing flex source
+struct CgFlexSource {
+    pool: Vec<u8>,
+    allocated: usize,
+}
+
+impl fmt::Debug for CgFlexSource {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("CgFlexSource")
+            .field("allocated", &self.allocated)
+            .finish()
+    }
+}
+
+impl Default for CgFlexSource {
+    fn default() -> Self {
+        Self {
+            pool: std::vec![0u8; 1024 * 32],
+            allocated: 0,
+        }
+    }
+}
+
+unsafe impl FlexSource for CgFlexSource {
+    unsafe fn alloc(&mut self, min_size: usize) -> Option<NonNull<[u8]>> {
+        let allocated = self.allocated;
+        let new_allocated = allocated
+            .checked_add(min_size)
+            .filter(|&x| x <= self.pool.len())?;
+
+        self.allocated = new_allocated;
+        Some(NonNull::from(&mut self.pool[allocated..new_allocated]))
+    }
+
+    unsafe fn realloc_inplace_grow(
+        &mut self,
+        ptr: NonNull<[u8]>,
+        min_new_len: usize,
+    ) -> Option<usize> {
+        self.alloc(min_new_len - nonnull_slice_len(ptr))
+            .map(|s| nonnull_slice_len(s) + nonnull_slice_len(ptr))
+    }
+
+    fn is_contiguous_growable(&self) -> bool {
+        true
+    }
+
+    fn min_align(&self) -> usize {
+        1
+    }
+}
+
 macro_rules! gen_test {
-    ($mod:ident, $($tt:tt)*) => {
+    ($mod:ident, $source:ty, $($tt:tt)*) => {
         mod $mod {
             use super::*;
-            type TheTlsf<'a> = FlexTlsf<TrackingFlexSource<GlobalAllocAsFlexSource<std::alloc::System, 1024>>, $($tt)*>;
+            type TheTlsf = FlexTlsf<TrackingFlexSource<$source>, $($tt)*>;
 
             #[test]
             fn minimal() {
@@ -174,33 +226,65 @@ macro_rules! gen_test {
     };
 }
 
-gen_test!(tlsf_u8_u8_1_1, u8, u8, 1, 1);
-gen_test!(tlsf_u8_u8_1_2, u8, u8, 1, 2);
-gen_test!(tlsf_u8_u8_1_4, u8, u8, 1, 4);
-gen_test!(tlsf_u8_u8_1_8, u8, u8, 1, 8);
-gen_test!(tlsf_u8_u8_3_4, u8, u8, 3, 4);
-gen_test!(tlsf_u8_u8_5_4, u8, u8, 5, 4);
-gen_test!(tlsf_u8_u8_8_1, u8, u8, 8, 1);
-gen_test!(tlsf_u8_u8_8_8, u8, u8, 8, 8);
-gen_test!(tlsf_u16_u8_3_4, u16, u8, 3, 4);
-gen_test!(tlsf_u16_u8_11_4, u16, u8, 11, 4);
-gen_test!(tlsf_u16_u8_16_4, u16, u8, 16, 4);
-gen_test!(tlsf_u16_u16_3_16, u16, u16, 3, 16);
-gen_test!(tlsf_u16_u16_11_16, u16, u16, 11, 16);
-gen_test!(tlsf_u16_u16_16_16, u16, u16, 16, 16);
-gen_test!(tlsf_u16_u32_3_16, u16, u32, 3, 16);
-gen_test!(tlsf_u16_u32_11_16, u16, u32, 11, 16);
-gen_test!(tlsf_u16_u32_16_16, u16, u32, 16, 16);
-gen_test!(tlsf_u16_u32_3_32, u16, u32, 3, 32);
-gen_test!(tlsf_u16_u32_11_32, u16, u32, 11, 32);
-gen_test!(tlsf_u16_u32_16_32, u16, u32, 16, 32);
-gen_test!(tlsf_u32_u32_20_32, u32, u32, 20, 32);
-gen_test!(tlsf_u32_u32_27_32, u32, u32, 27, 32);
-gen_test!(tlsf_u32_u32_28_32, u32, u32, 28, 32);
-gen_test!(tlsf_u32_u32_29_32, u32, u32, 29, 32);
-gen_test!(tlsf_u32_u32_32_32, u32, u32, 32, 32);
-gen_test!(tlsf_u64_u8_58_64, u64, u64, 58, 8);
-gen_test!(tlsf_u64_u8_59_64, u64, u64, 59, 8);
-gen_test!(tlsf_u64_u8_60_64, u64, u64, 60, 8);
-gen_test!(tlsf_u64_u8_61_64, u64, u64, 61, 8);
-gen_test!(tlsf_u64_u8_64_64, u64, u64, 64, 8);
+type SysSource = GlobalAllocAsFlexSource<std::alloc::System, 1024>;
+gen_test!(tlsf_sys_u8_u8_1_1, SysSource, u8, u8, 1, 1);
+gen_test!(tlsf_sys_u8_u8_1_2, SysSource, u8, u8, 1, 2);
+gen_test!(tlsf_sys_u8_u8_1_4, SysSource, u8, u8, 1, 4);
+gen_test!(tlsf_sys_u8_u8_1_8, SysSource, u8, u8, 1, 8);
+gen_test!(tlsf_sys_u8_u8_3_4, SysSource, u8, u8, 3, 4);
+gen_test!(tlsf_sys_u8_u8_5_4, SysSource, u8, u8, 5, 4);
+gen_test!(tlsf_sys_u8_u8_8_1, SysSource, u8, u8, 8, 1);
+gen_test!(tlsf_sys_u8_u8_8_8, SysSource, u8, u8, 8, 8);
+gen_test!(tlsf_sys_u16_u8_3_4, SysSource, u16, u8, 3, 4);
+gen_test!(tlsf_sys_u16_u8_11_4, SysSource, u16, u8, 11, 4);
+gen_test!(tlsf_sys_u16_u8_16_4, SysSource, u16, u8, 16, 4);
+gen_test!(tlsf_sys_u16_u16_3_16, SysSource, u16, u16, 3, 16);
+gen_test!(tlsf_sys_u16_u16_11_16, SysSource, u16, u16, 11, 16);
+gen_test!(tlsf_sys_u16_u16_16_16, SysSource, u16, u16, 16, 16);
+gen_test!(tlsf_sys_u16_u32_3_16, SysSource, u16, u32, 3, 16);
+gen_test!(tlsf_sys_u16_u32_11_16, SysSource, u16, u32, 11, 16);
+gen_test!(tlsf_sys_u16_u32_16_16, SysSource, u16, u32, 16, 16);
+gen_test!(tlsf_sys_u16_u32_3_32, SysSource, u16, u32, 3, 32);
+gen_test!(tlsf_sys_u16_u32_11_32, SysSource, u16, u32, 11, 32);
+gen_test!(tlsf_sys_u16_u32_16_32, SysSource, u16, u32, 16, 32);
+gen_test!(tlsf_sys_u32_u32_20_32, SysSource, u32, u32, 20, 32);
+gen_test!(tlsf_sys_u32_u32_27_32, SysSource, u32, u32, 27, 32);
+gen_test!(tlsf_sys_u32_u32_28_32, SysSource, u32, u32, 28, 32);
+gen_test!(tlsf_sys_u32_u32_29_32, SysSource, u32, u32, 29, 32);
+gen_test!(tlsf_sys_u32_u32_32_32, SysSource, u32, u32, 32, 32);
+gen_test!(tlsf_sys_u64_u8_58_64, SysSource, u64, u64, 58, 8);
+gen_test!(tlsf_sys_u64_u8_59_64, SysSource, u64, u64, 59, 8);
+gen_test!(tlsf_sys_u64_u8_60_64, SysSource, u64, u64, 60, 8);
+gen_test!(tlsf_sys_u64_u8_61_64, SysSource, u64, u64, 61, 8);
+gen_test!(tlsf_sys_u64_u8_64_64, SysSource, u64, u64, 64, 8);
+
+gen_test!(tlsf_cg_u8_u8_1_1, CgFlexSource, u8, u8, 1, 1);
+gen_test!(tlsf_cg_u8_u8_1_2, CgFlexSource, u8, u8, 1, 2);
+gen_test!(tlsf_cg_u8_u8_1_4, CgFlexSource, u8, u8, 1, 4);
+gen_test!(tlsf_cg_u8_u8_1_8, CgFlexSource, u8, u8, 1, 8);
+gen_test!(tlsf_cg_u8_u8_3_4, CgFlexSource, u8, u8, 3, 4);
+gen_test!(tlsf_cg_u8_u8_5_4, CgFlexSource, u8, u8, 5, 4);
+gen_test!(tlsf_cg_u8_u8_8_1, CgFlexSource, u8, u8, 8, 1);
+gen_test!(tlsf_cg_u8_u8_8_8, CgFlexSource, u8, u8, 8, 8);
+gen_test!(tlsf_cg_u16_u8_3_4, CgFlexSource, u16, u8, 3, 4);
+gen_test!(tlsf_cg_u16_u8_11_4, CgFlexSource, u16, u8, 11, 4);
+gen_test!(tlsf_cg_u16_u8_16_4, CgFlexSource, u16, u8, 16, 4);
+gen_test!(tlsf_cg_u16_u16_3_16, CgFlexSource, u16, u16, 3, 16);
+gen_test!(tlsf_cg_u16_u16_11_16, CgFlexSource, u16, u16, 11, 16);
+gen_test!(tlsf_cg_u16_u16_16_16, CgFlexSource, u16, u16, 16, 16);
+gen_test!(tlsf_cg_u16_u32_3_16, CgFlexSource, u16, u32, 3, 16);
+gen_test!(tlsf_cg_u16_u32_11_16, CgFlexSource, u16, u32, 11, 16);
+gen_test!(tlsf_cg_u16_u32_16_16, CgFlexSource, u16, u32, 16, 16);
+gen_test!(tlsf_cg_u16_u32_3_32, CgFlexSource, u16, u32, 3, 32);
+gen_test!(tlsf_cg_u16_u32_11_32, CgFlexSource, u16, u32, 11, 32);
+gen_test!(tlsf_cg_u16_u32_16_32, CgFlexSource, u16, u32, 16, 32);
+gen_test!(tlsf_cg_u32_u32_20_32, CgFlexSource, u32, u32, 20, 32);
+gen_test!(tlsf_cg_u32_u32_27_32, CgFlexSource, u32, u32, 27, 32);
+gen_test!(tlsf_cg_u32_u32_28_32, CgFlexSource, u32, u32, 28, 32);
+gen_test!(tlsf_cg_u32_u32_29_32, CgFlexSource, u32, u32, 29, 32);
+gen_test!(tlsf_cg_u32_u32_32_32, CgFlexSource, u32, u32, 32, 32);
+gen_test!(tlsf_cg_u64_u8_58_64, CgFlexSource, u64, u64, 58, 8);
+gen_test!(tlsf_cg_u64_u8_59_64, CgFlexSource, u64, u64, 59, 8);
+gen_test!(tlsf_cg_u64_u8_60_64, CgFlexSource, u64, u64, 60, 8);
+gen_test!(tlsf_cg_u64_u8_61_64, CgFlexSource, u64, u64, 61, 8);
+gen_test!(tlsf_cg_u64_u8_64_64, CgFlexSource, u64, u64, 64, 8);
