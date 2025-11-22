1c50c5ff286f3accc9bca4991891a71d5296f98c
yvt <i@yvt.jp>
2025-11-22 14:38:14 +0900

    fix(utils): use `UnsafeCell` in temporary reference created by `nonnull_slice_len`
    > [..] because [..] there might be outstanding mutable references to the
    > slice.
    
    <https://doc.rust-lang.org/reference/behavior-considered-undefined.html>
    
    > The exact aliasing rules are not determined yet, but here is an
    > outline of the general principles: `&T` must point to memory that is
    > not mutated while they are live (except for data inside an
    > `UnsafeCell<U>`),

--- crates/rlsf/src/utils.rs
+++ crates/rlsf/src/utils.rs
@@ -1,4 +1,4 @@
-use core::{mem::MaybeUninit, ptr::NonNull};
+use core::{cell::UnsafeCell, mem::MaybeUninit, ptr::NonNull};
 
 /// Polyfill for <https://github.com/rust-lang/rust/issues/71941>
 #[inline]
@@ -9,11 +9,13 @@ pub fn nonnull_slice_from_raw_parts<T>(ptr: NonNull<T>, len: usize) -> NonNull<[
 /// Polyfill for  <https://github.com/rust-lang/rust/issues/71146>
 #[inline]
 pub fn nonnull_slice_len<T>(ptr: NonNull<[T]>) -> usize {
+    // FIXME: Use `NonNull<[T]>::len` (stabilized in Rust 1.63)
     // Safety: We are just reading the slice length embedded in the fat
     //         pointer and not dereferencing the pointer. We also convert it
-    //         to `*mut [MaybeUninit<u8>]` just in case because the slice
-    //         might be uninitialized.
-    unsafe { (*(ptr.as_ptr() as *const [MaybeUninit<T>])).len() }
+    //         to `*mut [MaybeUninit<UnsafeCell<u8>>]` just in case because the
+    //         slice might be uninitialized and there might be outstanding
+    //         mutable references to the slice.
+    unsafe { (&*(ptr.as_ptr() as *const [MaybeUninit<UnsafeCell<T>>])).len() }
 }
 
 // Polyfill for <https://github.com/rust-lang/rust/issues/74265>
